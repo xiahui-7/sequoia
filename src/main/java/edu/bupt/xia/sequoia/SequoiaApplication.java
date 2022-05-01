66ab55c4f7f8026d313874dce4ab009860d2c1f2
xiahui <515065283@qq.com>
2022-05-01 12:13:53 +0800

    Initial commit

---
+++ src/main/java/edu/bupt/xia/sequoia/SequoiaApplication.java
@@ -0,0 +1,19 @@
+package edu.bupt.xia.sequoia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * spring boot starter
+ *
+ * @author xiahui
+ * @date 2022/5/1 11:59
+ */
+@SpringBootApplication
+public class SequoiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SequoiaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/bupt/xia/sequoia/SequoiaApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.bupt.xia.sequoia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SequoiaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
