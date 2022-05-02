e72b82a2d1ae7ae5a4bb3c1c1f7581d7b48efbec
xiahui <515065283@qq.com>
2022-05-02 16:27:29 +0800

    fix: add base path

--- src/main/java/edu/bupt/xia/sequoia/controller/ShortUrlController.java
+++ src/main/java/edu/bupt/xia/sequoia/controller/ShortUrlController.java
@@ -4,13 +4,15 @@ import edu.bupt.xia.sequoia.service.ShortUrlService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author xiahui
  * @date 2022/5/1 13:01
  */
-@RestController("/short/url")
+@RestController
+@RequestMapping("/short/url")
 public class ShortUrlController {
     private final ShortUrlService shortUrlService;
 
